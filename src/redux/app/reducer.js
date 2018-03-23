96e495389ccb6726fd33350b1484d705bbd0a7f0
iam2611008 <dragonwarrior1008@yandex.com>
2018-03-23 07:42:12 -0700

    Update redux functions

--- src/redux/app/reducer.js
+++ src/redux/app/reducer.js
@@ -2,13 +2,26 @@ import { Map } from 'immutable';
 import actions from './actions';
 
 const initState = new Map({
-  children: []
+  children: [],
+  selectedChildren: null,
+  assessments: [],
+  questionnaires: [],
+  current_questionnaire_step: 0
 });
 
 export default function appReducer(state = initState, action) {
   switch (action.type) {
+    case actions.SELECT_CHILDREN:
+      return state.set('selectedChildren', action.payload);
     case actions.GET_CHILDREN_SUCCESS:
-      return state.set('children', action.children);      
+      return state.set('children', action.children);   
+    case actions.GET_ASSESSMENTS_SUCCESS:
+    	return state.set('assessments', action.assessments);
+    case actions.GET_QUESTIONNAIRES_SUCCESS:
+      return state.set('questionnaires', action.questionnaires);
+    case actions.SELECT_ANSWER:
+      let nextStep = state.toJS().current_questionnaire_step + 1;
+      return state.set('current_questionnaire_step', nextStep);
     default:
       return state;
   }
--- src/redux/app/saga.js
+++ src/redux/app/saga.js
@@ -3,7 +3,7 @@ import { push } from 'react-router-redux';
 import axios from 'axios';
 
 import { clearToken } from '../../helpers/utility';
-import { listChildrenAPI } from '../../helpers/apis';
+import { listChildrenAPI, listAssessmentsAPI, getQuestionnaireAPI, listOutstandingAssessmentsAPI } from '../../helpers/apis';
 import actions from './actions';
 
 export function* getChildren() {
@@ -27,12 +27,76 @@ export function* getChildren() {
   });
 }
 
-export function* getChildrenFail() {
-  yield takeEvery(actions.GET_CHILDREN_FAIL, function*() {});
+export function* getAssessments() {
+  yield takeEvery('GET_ASSESSMENTS_REQUEST', function*() {
+    let assessments;
+
+    try {
+      assessments = yield call(listAssessmentsAPI);
+      
+      yield put({
+        type: actions.GET_ASSESSMENTS_SUCCESS,
+        assessments: assessments
+      });
+    }
+    catch (error) {
+      yield put({
+        type: actions.GET_ASSESSMENTS_FAIL
+      });
+    }
+  });
+}
+
+export function* selectChildren() {
+  yield takeEvery('SELECT_CHILDREN', function*(payload) {
+    let outstanding_assessments;
+
+    try {
+      outstanding_assessments = yield call(listOutstandingAssessmentsAPI, payload.selectedChildren);
+      
+      yield put({
+        type: actions.GET_OUTSTANDING_ASSESSMENTS_SUCCESS,
+        assessments: outstanding_assessments
+      });
+
+      yield put(push('/assessments'));
+    }
+    catch (error) {
+      yield put({
+        type: actions.GET_OUTSTANDING_ASSESSMENTS_FAILED
+      });
+    }
+    
+  });
+}
+
+export function* selectAssessment() {
+  yield takeEvery('SELECT_ASSESSMENT', function*(payload) {
+    let questionnaires;
+    try {
+      questionnaires = yield call(getQuestionnaireAPI, payload.assessment.id);
+
+      yield put({
+        type: actions.GET_QUESTIONNAIRES_SUCCESS,
+        questionnaires
+      });
+
+      yield put(push('/questionnaires'));
+    }
+    catch(error) {
+      yield put({
+        type: actions.GET_QUESTIONNAIRES_FAIL
+      });
+    }
+    
+  });
 }
 
 export default function* rootSaga() {
   yield all([
-    fork(getChildren)
+    fork(getChildren),
+    fork(getAssessments),
+    fork(selectChildren),
+    fork(selectAssessment)
   ]);
 }
