2bbda921a3c1e486020e1a9564148e16379929ac
Abhiisith Phanlert <dragonwarrior1008@yandex.com>
2018-03-29 08:56:12 -0700

    Create reducer storing question responses in JSON

--- src/redux/app/reducer.js
+++ src/redux/app/reducer.js
@@ -47,8 +47,13 @@ const initState = new Map({
       }
     ]},
   assessments: [],
+  selectedQuestionnaireID: null,
   questionnaires: [],
-  current_questionnaire_step: 1
+  current_questionnaire_step: 1,
+  questionnaire_responses: {
+    ids: [],
+    names: []
+  }
 });
 
 export default function appReducer(state = initState, action) {
@@ -58,14 +63,26 @@ export default function appReducer(state = initState, action) {
         .set('selectedChildren', action.children)
         .set('current_questionnaire_step', 1);
     case actions.GET_CHILDREN_SUCCESS:
-      return state.set('children', action.children);   
+      return state.set('children', action.children); 
+    case actions.SELECT_ASSESSMENT:
+      console.log(action.assessment);
+      return state
+        .set('selectedQuestionnaireID', action.assessment.id)
+        .set('questionnaire_responses', { ids: [], names: []});
     case actions.GET_ASSESSMENTS_SUCCESS:
     	return state.set('assessments', action.assessments);
     case actions.GET_QUESTIONNAIRES_SUCCESS:
       return state.set('questionnaires', action.questionnaires);
     case actions.SELECT_ANSWER:
       let nextStep = state.toJS().current_questionnaire_step + 1;
-      return state.set('current_questionnaire_step', nextStep);
+      state.toJS().questionnaire_responses.ids.push({ question_response_id: action.payload.question_response.id, question_id: action.payload.question.id});
+      state.toJS().questionnaire_responses.names.push({ question_response_name: action.payload.question_response.name, question_title: action.payload.question.title});
+      return state
+        .set('current_questionnaire_step', nextStep);
+    case actions.SELECT_LAST_ANSWER:
+      state.toJS().questionnaire_responses.ids.push({ question_response_id: action.payload.question_response.id, question_id: action.payload.question.id});
+      state.toJS().questionnaire_responses.names.push({ question_response_name: action.payload.question_response.name, question_title: action.payload.question.title});
+      return state;
     case actions.PREVIOUS_QUESTION:
       let prevStep = state.toJS().current_questionnaire_step - 1;
       return state.set('current_questionnaire_step', prevStep);
