58a21645d48c7739d949af9c796cdcbec6a3961b
Abhiisith Phanlert <dragonwarrior1008@yandex.com>
2018-03-29 08:54:11 -0700

    Update progress bar as component

---
+++ src/components/Questionnaires/progressBar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ProgressBarWrapper = styled.div`
+	width: 200px;
+	height: 20px;
+	background-color: white;
+	border-radius: 20px;
+	overflow: hidden;
+	.current-progress {
+		width: ${props => (props.percentage / props.questionLength) * 100}%;
+		min-width: 40px;
+		height: 20px;
+		background-color: #0089ff;
+		border-radius: 20px;
+		text-align: center;
+		p {
+			margin: 0;
+			font-size: 0.8em;
+			line-height: 20px;
+		}
+	}
+`;
+
+const ProgressBar = ({percentage, questionLength}) => (
+  <ProgressBarWrapper percentage={percentage>1 ? percentage : 1} questionLength={questionLength}>
+    <div className="current-progress">
+      <p>{percentage}/{questionLength}</p>
+    </div>
+  </ProgressBarWrapper>
+);
+
+export default ProgressBar;
