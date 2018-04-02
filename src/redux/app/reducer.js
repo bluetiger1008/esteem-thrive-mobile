5207548ced47a3cd1cce89bdb97661f4708b9c1b
iam2611008 <dragonwarrior1008@yandex.com>
2018-04-02 11:47:02 -0700

    Add completed flag to questionnaire redux state on completed_questionnaire_success

--- src/redux/app/reducer.js
+++ src/redux/app/reducer.js
@@ -1,6 +1,7 @@
-import { Map } from 'immutable';
+import { Map, merge } from 'immutable';
 import actions from './actions';
 import { push } from 'react-router-redux';
+import _ from 'lodash';
 
 const initState = new Map({
   children: [],
@@ -62,27 +63,34 @@ export default function appReducer(state = initState, action) {
       return state
         .set('selectedChildren', action.children)
         .set('current_questionnaire_step', 1);
+
     case actions.GET_CHILDREN_SUCCESS:
       return state.set('children', action.children); 
+
     case actions.SELECT_ASSESSMENT:
       console.log(action.assessment);
       return state
         .set('selectedQuestionnaireID', action.assessment.id)
         .set('questionnaire_responses', { ids: [], names: []});
+
     case actions.GET_ASSESSMENTS_SUCCESS:
     	return state.set('assessments', action.assessments);
+
     case actions.GET_QUESTIONNAIRES_SUCCESS:
       return state.set('questionnaires', action.questionnaires);
+
     case actions.SELECT_ANSWER:
       let nextStep = state.toJS().current_questionnaire_step + 1;
       state.toJS().questionnaire_responses.ids.push({ question_response_id: action.payload.question_response.id, question_id: action.payload.question.id});
       state.toJS().questionnaire_responses.names.push({ question_response_name: action.payload.question_response.name, question_title: action.payload.question.title});
       return state
         .set('current_questionnaire_step', nextStep);
+
     case actions.SELECT_LAST_ANSWER:
       state.toJS().questionnaire_responses.ids.push({ question_response_id: action.payload.question_response.id, question_id: action.payload.question.id});
       state.toJS().questionnaire_responses.names.push({ question_response_name: action.payload.question_response.name, question_title: action.payload.question.title});
       return state;
+
     case actions.PREVIOUS_QUESTION:
       let prevStep = state.toJS().current_questionnaire_step - 1;
       return state.set('current_questionnaire_step', prevStep);
@@ -91,6 +99,10 @@ export default function appReducer(state = initState, action) {
       return state
         .set('questionnaire_responses', { ids: [], names: []})
         .set('current_questionnaire_step', 1);
+
+    case actions.COMPLETED_QUESTIONNAIRES_SUCCESS:
+      _.find(state.toJS().selectedChildren.questionnaires, { id: action.assessment.questionnaire_id }).completed = true;
+      return state;
     default:
       return state;
   }
