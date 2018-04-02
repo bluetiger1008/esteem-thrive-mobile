aeb9f411e2d833c722be46440a70278954126fca
iam2611008 <dragonwarrior1008@yandex.com>
2018-04-02 11:47:41 -0700

    Add redux function call for continueAssessment

--- src/components/Questionnaires/completed.js
+++ src/components/Questionnaires/completed.js
@@ -8,7 +8,7 @@ import appActions from '../../redux/app/actions';
 import authActions from '../../redux/auth/actions';
 import _ from 'lodash';
 
-const { get_assessments, get_children, select_assessment } = appActions;
+const { get_assessments, get_children, select_assessment, continue_assessments } = appActions;
 const { logout } = authActions;
 
 class QuestionnaireCompleted extends Component {
@@ -39,6 +39,11 @@ class QuestionnaireCompleted extends Component {
     this.props.logout();
   }
 
+  continueAssessment = () => {
+    console.log('continue assessment');
+    this.props.continue_assessments();
+  }
+
 	render() {
 		const { selectedChildren, questionnaires, children } = this.props;
     const { remainingAssessmentNumber } = this.state;
@@ -52,7 +57,7 @@ class QuestionnaireCompleted extends Component {
         <Div className="content" direction="column" alignItems="center">
           <h2>{questionnaires.title}</h2>
           <h2>Completed</h2>
-          <button className="btn btn-continue">
+          <button className="btn btn-continue" onClick={this.continueAssessment}>
             Continue
             <p className="due-notification">{remainingAssessmentNumber}</p>
           </button>
@@ -69,5 +74,5 @@ export default connect(
   state => ({
     ...state.App.toJS()
   }),
-  { get_assessments, get_children, select_assessment, logout }
+  { get_assessments, get_children, select_assessment, continue_assessments, logout }
 )(QuestionnaireCompleted);
