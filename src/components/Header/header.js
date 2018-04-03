c67a6cae646b2a713848eaf9643abfeb68a804ee
iam2611008 <dragonwarrior1008@yandex.com>
2018-04-03 12:26:08 -0700

    Create Header component

---
+++ src/components/Header/header.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import 'font-awesome/css/font-awesome.css';
+import FontAwesome from 'react-fontawesome';
+import { Link, Redirect } from 'react-router-dom';
+
+import { Div } from '../common';
+import logo from '../../assets/images/logo_black.png';
+import HeaderWrapper from './header.style';
+
+class Header extends Component {
+	render() {
+		const { selectedChildren } = this.props;
+
+		return (
+			<HeaderWrapper>
+				<Div className="header" direction="column" justifyContent="center" alignItems="center">
+					<Link to="/children" className="btn-back">
+            <FontAwesome
+			        name='angle-left'
+			        size='2x'
+			      />
+          </Link>
+					<p>{selectedChildren.name}</p>
+					<img src={logo} alt="logo" className="logo"/>
+				</Div>
+			</HeaderWrapper>
+		);
+	}
+}
+
+export default connect(
+  state => ({
+    ...state.App.toJS()
+  }),
+  { }
+)(Header);
