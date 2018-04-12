1ac25ec4f6f9c5ee3cbc45b6e07a2bcd395a89cc
Jamie McElwain <jgmcelwain@icloud.com>
2018-04-12 09:42:57 +0100

    style: small adjustments to button font in index

--- src/components/Children/index.js
+++ src/components/Children/index.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import logo from '../../assets/images/logo_black.png';
-import ChildrenWrapper from './index.style';
-import { Div } from '../common';
-import appActions from '../../redux/app/actions';
-import authActions from '../../redux/auth/actions';
+import logo from '../../assets/images/logo_black.png'
+import ChildrenWrapper from './index.style'
+import { Div } from '../common'
+import appActions from '../../redux/app/actions'
+import authActions from '../../redux/auth/actions'
 
-const { get_children, select_children } = appActions;
-const { logout } = authActions;
+const { get_children, select_children } = appActions
+const { logout } = authActions
 
 class Children extends Component {
   componentDidMount() {
@@ -19,35 +19,35 @@ class Children extends Component {
     this.props.select_children(item)
   }
 
-	logout = () => {
-    this.props.logout();
+  logout = () => {
+    this.props.logout()
   }
 
-	render() {
-		const { children } = this.props;
+  render() {
+    const { children } = this.props
 
-		return (
-			<ChildrenWrapper>
-				<Div className="header" direction="column" justifyContent="flex-end">
-					<h2>Notifications App</h2>
-					<p>Designed to provide easy access to notifications, ask an expert and mom on board.</p>
-					<img src={logo} alt="logo" className="logo"/>
-				</Div>
-				<Div className="content" direction="column" alignItems="center" justifyContent="flex-start">
-					{ children.map((item, index) =>
-						<button className="btn-notify" key={index} onClick={this.onSelectChildren.bind(undefined, item)}>
-							{ item.name }
-							<p className="due-notification">{item.outstanding_assessments_size}</p>
-						</button>
-					)}
-					
-					<a className="btn-logout" onClick={this.logout}>
-					 	Logout
-					</a>
-				</Div>
-			</ChildrenWrapper>
-		);
-	}
+    return (
+      <ChildrenWrapper>
+        <Div className="header" direction="column" justifyContent="flex-end">
+          <h2>Notifications App</h2>
+          <p>Designed to provide easy access to notifications, ask an expert and mom on board.</p>
+          <img src={logo} alt="logo" className="logo" />
+        </Div>
+        <Div className="content" direction="column" alignItems="center" justifyContent="flex-start">
+          {children.map((item, index) => (
+            <button className="btn-notify" key={index} onClick={this.onSelectChildren.bind(undefined, item)}>
+              {item.name}
+              <p className="due-notification">{item.outstanding_assessments_size}</p>
+            </button>
+          ))}
+
+          <a className="btn-logout" onClick={this.logout}>
+            Logout
+          </a>
+        </Div>
+      </ChildrenWrapper>
+    )
+  }
 }
 
 export default connect(
@@ -55,4 +55,4 @@ export default connect(
     ...state.App.toJS()
   }),
   { get_children, select_children, logout }
-)(Children);
+)(Children)
--- src/components/Children/index.style.js
+++ src/components/Children/index.style.js
@@ -3,68 +3,70 @@ import bgMountain from '../../assets/images/mountain.png'
 
 const ChildrenWraper = styled.div`
   .header {
-		border-bottom: 1px solid grey;
-		padding: 0 30px;
-		height: 150px;
-		text-align: left;
-		position: relative;
-		h2 {
-			margin: 0;
-			color: #5d5b5b;
-		}
-		p {
-			color: #5d5b5b;
-		}
-		.logo {
-			position: absolute;
-			right: 10px;
-			top: 10px;
-			width: 80px;
-		}
-	}
-	.content {
-		height: calc(100vh - 151px);
-		width: 100%;
-		background-image: url(${bgMountain});
+    border-bottom: 1px solid grey;
+    padding: 0 30px;
+    height: 150px;
+    text-align: left;
+    position: relative;
+    h2 {
+      margin: 0;
+      color: #5d5b5b;
+    }
+    p {
+      color: #5d5b5b;
+    }
+    .logo {
+      position: absolute;
+      right: 10px;
+      top: 10px;
+      width: 80px;
+    }
+  }
+  .content {
+    height: calc(100vh - 151px);
+    width: 100%;
+    background-image: url(${bgMountain});
     background-size: 100% auto;
     background-repeat: no-repeat;
     background-position: bottom;
     button {
-    	&.btn-notify {
-    		width: 200px;
-    		height: 50px;
-    		border-radius: 80px;
-    		background-color: #ecb609;
-    		margin: 10px 0;
-    		color: white;
-    		font-size: 20px;
-    		outline: none;
-    		border: none;
-    		position: relative;
-    		&:first-child {
-    			margin-top: 100px;
-    		}
-    		.due-notification {
-    			position: absolute;
-    			width: 30px;
-    			height: 30px;
-    			right: 0px;
-    			top: -15px;
-    			background-color: #000;
-    			border-radius: 50%;
-    			margin: 0;
-    			line-height: 30px;
-    			border: 3px solid #fff;
-    		}
-    	}
+      &.btn-notify {
+        width: 200px;
+        height: 50px;
+        border-radius: 80px;
+        background-color: #ecb609;
+        margin: 10px 0;
+        color: white;
+        font-size: 20px;
+        outline: none;
+        border: none;
+        position: relative;
+        cursor: pointer;
+        font-family: 'Inter UI', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        &:first-child {
+          margin-top: 100px;
+        }
+        .due-notification {
+          position: absolute;
+          width: 30px;
+          height: 30px;
+          right: 0px;
+          top: -15px;
+          background-color: #000;
+          border-radius: 50%;
+          margin: 0;
+          line-height: 30px;
+          border: 3px solid #fff;
+        }
+      }
     }
     .btn-logout {
-            font-size: 20px;
-            color: black;
-            text-decoration: underline;
-            margin-top: 50px;
+      font-size: 20px;
+      color: black;
+      text-decoration: underline;
+      margin-top: 50px;
     }
-	}
-`;
+  }
+`
 
 export default ChildrenWraper
